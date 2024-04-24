8e3b40da27f82e54c25cf0f7c892092bcc77bc68
SHEAU-JWU WU <146037423+sjosk@users.noreply.github.com>
2024-04-24 23:00:59 +0100

    Create nano_ble33_sense_fusionV3.ino

---
+++ Code/nano_ble33_sense_fusionV3/nano_ble33_sense_fusionV3.ino
@@ -0,0 +1,354 @@
+/* Edge Impulse ingestion SDK
+ * Copyright (c) 2022 EdgeImpulse Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* Includes ---------------------------------------------------------------- */
+#include <Gesture-Controlled_Traffic_Baton_v3_inferencing.h>
+#include <Arduino_LSM9DS1.h> //Click here to get the library: https://www.arduino.cc/reference/en/libraries/arduino_lsm9ds1/
+#include <LedControl.h>
+
+int DIN_PIN = 7;  
+int CLK_PIN = 6;  
+int CS_PIN = 5;   
+int numDevices = 1;
+LedControl lc = LedControl(DIN_PIN, CLK_PIN, CS_PIN, numDevices);
+
+enum sensor_status {
+    NOT_USED = -1,
+    NOT_INIT,
+    INIT,
+    SAMPLED
+};
+
+/** Struct to link sensor axis name to sensor value function */
+typedef struct{
+    const char *name;
+    float *value;
+    uint8_t (*poll_sensor)(void);
+    bool (*init_sensor)(void);    
+    sensor_status status;
+} eiSensors;
+
+/* Constant defines -------------------------------------------------------- */
+#define CONVERT_G_TO_MS2    9.80665f
+#define MAX_ACCEPTED_RANGE  2.0f        // starting 03/2022, models are generated setting range to +-2,
+                                        // but this example use Arudino library which set range to +-4g. 
+                                        // If you are using an older model, ignore this value and use 4.0f instead
+/** Number sensor axes used */
+#define N_SENSORS     9
+
+/* Forward declarations ------------------------------------------------------- */
+float ei_get_sign(float number);
+
+bool init_IMU(void);
+bool init_HTS(void);
+bool init_BARO(void);
+bool init_APDS(void);
+
+uint8_t poll_acc(void);
+uint8_t poll_gyr(void);
+uint8_t poll_mag(void);
+
+
+/* Private variables ------------------------------------------------------- */
+static const bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
+
+static float data[N_SENSORS];
+static bool ei_connect_fusion_list(const char *input_list);
+
+static int8_t fusion_sensors[N_SENSORS];
+static int fusion_ix = 0;
+
+/** Used sensors value function connected to label name */
+eiSensors sensors[] =
+{
+    "accX", &data[0], &poll_acc, &init_IMU, NOT_USED,
+    "accY", &data[1], &poll_acc, &init_IMU, NOT_USED,
+    "accZ", &data[2], &poll_acc, &init_IMU, NOT_USED,
+    "gyrX", &data[3], &poll_gyr, &init_IMU, NOT_USED,
+    "gyrY", &data[4], &poll_gyr, &init_IMU, NOT_USED,
+    "gyrZ", &data[5], &poll_gyr, &init_IMU, NOT_USED,
+    "magX", &data[6], &poll_mag, &init_IMU, NOT_USED,
+    "magY", &data[7], &poll_mag, &init_IMU, NOT_USED,
+    "magZ", &data[8], &poll_mag, &init_IMU, NOT_USED,
+
+};
+
+/**
+* @brief      Arduino setup function
+*/
+void setup()
+{
+    lc.shutdown(0, false);       // Wake up displays
+    lc.setIntensity(0, 8);       // Set brightness level (0 is min, 15 is max)
+    lc.clearDisplay(0);          // Clear display register
+    /* Init serial */
+    Serial.begin(115200);
+    // comment out the below line to cancel the wait for USB connection (needed for native USB)
+    while (!Serial);
+    Serial.println("Edge Impulse Sensor Fusion Inference\r\n");
+
+    /* Connect used sensors */
+    if(ei_connect_fusion_list(EI_CLASSIFIER_FUSION_AXES_STRING) == false) {
+        ei_printf("ERR: Errors in sensor list detected\r\n");
+        return;
+    }
+
+    /* Init & start sensors */
+
+    for(int i = 0; i < fusion_ix; i++) {
+        if (sensors[fusion_sensors[i]].status == NOT_INIT) {
+            sensors[fusion_sensors[i]].status = (sensor_status)sensors[fusion_sensors[i]].init_sensor();
+            if (!sensors[fusion_sensors[i]].status) {
+              ei_printf("%s axis sensor initialization failed.\r\n", sensors[fusion_sensors[i]].name);             
+            }
+            else {
+              ei_printf("%s axis sensor initialization successful.\r\n", sensors[fusion_sensors[i]].name);
+            }
+        }
+    }
+}
+
+/**
+* @brief      Get data and run inferencing
+*/
+void loop() {
+    ei_printf("\nStarting inferencing in 2 seconds...\r\n");
+    delay(2000);
+
+    if (EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME != fusion_ix) {
+        ei_printf("ERR: Sensors don't match the sensors required in the model\r\n"
+        "Following sensors are required: %s\r\n", EI_CLASSIFIER_FUSION_AXES_STRING);
+        return;
+    }
+
+    ei_printf("Sampling...\r\n");
+    float buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE] = { 0 };
+
+    for (size_t ix = 0; ix < EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE; ix += EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME) {
+        int64_t next_tick = (int64_t)micros() + ((int64_t)EI_CLASSIFIER_INTERVAL_MS * 1000);
+        for(int i = 0; i < fusion_ix; i++) {
+            if (sensors[fusion_sensors[i]].status == INIT) {
+                sensors[fusion_sensors[i]].poll_sensor();
+                sensors[fusion_sensors[i]].status = SAMPLED;
+            }
+            if (sensors[fusion_sensors[i]].status == SAMPLED) {
+                buffer[ix + i] = *sensors[fusion_sensors[i]].value;
+                sensors[fusion_sensors[i]].status = INIT;
+            }
+        }
+        int64_t wait_time = next_tick - (int64_t)micros();
+        if(wait_time > 0) {
+            delayMicroseconds(wait_time);
+        }
+    }
+
+    // Turn the raw buffer into a signal that can be classified
+    signal_t signal;
+    int err = numpy::signal_from_buffer(buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
+    if (err != 0) {
+        ei_printf("ERR:(%d)\r\n", err);
+        return;
+    }
+
+    // Run the classifier
+    ei_impulse_result_t result = { 0 };
+    err = run_classifier(&signal, &result, debug_nn);
+    if (err != EI_IMPULSE_OK) {
+        ei_printf("ERR:(%d)\r\n", err);
+        return;
+    }
+
+    // Determine the highest scoring label that is above the threshold
+    float max_score = 0.5;  // Start with the threshold value
+    const char* max_label = "";
+    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+        if (result.classification[ix].value > max_score) {
+            max_score = result.classification[ix].value;
+            max_label = result.classification[ix].label;
+        }
+    }
+
+    if (max_score > 0.5) {
+    if (strcmp(max_label, "hit_r") == 0) {
+        displayRightArrow();
+    } else if (strcmp(max_label, "hit_c") == 0) {
+        displayStopSign();
+    } else if (strcmp(max_label, "wave_rtol") == 0) {
+        displayUpwardArrow();
+    }
+    
+    ei_printf("Highest scoring label above threshold: %s with confidence %.5f\r\n", max_label, max_score);
+} else {
+    ei_printf("No label scored above the threshold of 0.5\r\n");
+    lc.clearDisplay(0); // Clear the display if no valid label is detected
+}
+#if EI_CLASSIFIER_HAS_ANOMALY == 1
+    ei_printf("    anomaly score: %.3f\r\n", result.anomaly);
+#endif
+}
+
+
+/**
+ * @brief Go through sensor list to find matching axis name
+ *
+ * @param axis_name
+ * @return int8_t index in sensor list, -1 if axis name is not found
+ */
+static int8_t ei_find_axis(char *axis_name)
+{
+    int ix;
+    for(ix = 0; ix < N_SENSORS; ix++) {
+        if(strstr(axis_name, sensors[ix].name)) {
+            return ix;
+        }
+    }
+    return -1;
+}
+
+/**
+ * @brief Check if requested input list is valid sensor fusion, create sensor buffer
+ *
+ * @param[in]  input_list      Axes list to sample (ie. "accX + gyrY + magZ")
+ * @retval  false if invalid sensor_list
+ */
+static bool ei_connect_fusion_list(const char *input_list)
+{
+    char *buff;
+    bool is_fusion = false;
+
+    /* Copy const string in heap mem */
+    char *input_string = (char *)ei_malloc(strlen(input_list) + 1);
+    if (input_string == NULL) {
+        return false;
+    }
+    memset(input_string, 0, strlen(input_list) + 1);
+    strncpy(input_string, input_list, strlen(input_list));
+
+    /* Clear fusion sensor list */
+    memset(fusion_sensors, 0, N_SENSORS);
+    fusion_ix = 0;
+
+    buff = strtok(input_string, "+");
+
+    while (buff != NULL) { /* Run through buffer */
+        int8_t found_axis = 0;
+
+        is_fusion = false;
+        found_axis = ei_find_axis(buff);
+
+        if(found_axis >= 0) {
+            if(fusion_ix < N_SENSORS) {
+                fusion_sensors[fusion_ix++] = found_axis;
+                sensors[found_axis].status = NOT_INIT;
+            }
+            is_fusion = true;
+        }
+
+        buff = strtok(NULL, "+ ");
+    }
+
+    ei_free(input_string);
+
+    return is_fusion;
+}
+
+/**
+ * @brief Return the sign of the number
+ * 
+ * @param number 
+ * @return int 1 if positive (or 0) -1 if negative
+ */
+float ei_get_sign(float number) {
+    return (number >= 0.0) ? 1.0 : -1.0;
+}
+
+bool init_IMU(void) {
+  static bool init_status = false;
+  if (!init_status) {
+    init_status = IMU.begin();
+  }
+  return init_status;
+}
+
+
+uint8_t poll_acc(void) {
+  
+    if (IMU.accelerationAvailable()) {
+
+    IMU.readAcceleration(data[0], data[1], data[2]);
+
+    for (int i = 0; i < 3; i++) {
+        if (fabs(data[i]) > MAX_ACCEPTED_RANGE) {
+            data[i] = ei_get_sign(data[i]) * MAX_ACCEPTED_RANGE;
+        }
+    }
+
+    data[0] *= CONVERT_G_TO_MS2;
+    data[1] *= CONVERT_G_TO_MS2;
+    data[2] *= CONVERT_G_TO_MS2;
+    }
+
+    return 0;
+}
+
+uint8_t poll_gyr(void) {
+  
+    if (IMU.gyroscopeAvailable()) {
+        IMU.readGyroscope(data[3], data[4], data[5]);
+    }
+    return 0;
+}
+
+uint8_t poll_mag(void) {
+  
+    if (IMU.magneticFieldAvailable()) {
+        IMU.readMagneticField(data[6], data[7], data[8]);
+    }
+    return 0;
+}
+
+void displayRightArrow() {
+    lc.setRow(0, 0, B00100000);
+    lc.setRow(0, 1, B00110000);
+    lc.setRow(0, 2, B00011000);
+    lc.setRow(0, 3, B11111110);
+    lc.setRow(0, 4, B11111110);
+    lc.setRow(0, 5, B00011000);
+    lc.setRow(0, 6, B00110000);
+    lc.setRow(0, 7, B00100000);
+}
+
+void displayStopSign() {
+    lc.setRow(0, 0, B00111100); //  ****  
+    lc.setRow(0, 1, B01110010); // ***  * 
+    lc.setRow(0, 2, B10110001); //* **   *
+    lc.setRow(0, 3, B10011001); //*  **  *
+    lc.setRow(0, 4, B10001101); //*   ** *
+    lc.setRow(0, 5, B10001101); //*   ** *
+    lc.setRow(0, 6, B01001110); // *  ***  
+    lc.setRow(0, 7, B00111100); //  ****  
+}
+
+void displayUpwardArrow() {
+    lc.setRow(0, 7, B00011000);  //    **
+    lc.setRow(0, 6, B00111100);  //   ****
+    lc.setRow(0, 5, B01111110);  //  ******
+    lc.setRow(0, 4, B00011000);  //    **
+    lc.setRow(0, 3, B00011000);  //    **
+    lc.setRow(0, 2, B00011000);  //    **
+    lc.setRow(0, 1, B00011000);  //    **
+    lc.setRow(0, 0, B00011000);  //    **
+}
